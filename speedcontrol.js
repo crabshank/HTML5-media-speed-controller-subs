0433144069ad55ecbd315db34d497843e9f60298
crabshank <44329154+crabshank@users.noreply.github.com>
2020-06-17 19:32:36 +0100

    Add files via upload

--- speedcontrol.js
+++ speedcontrol.js
@@ -701,6 +701,10 @@ span.speed-indicator{
 		{
 			self.speedIndicator_.textContent = self.rmng();
 		});
+		this.videoEl_.addEventListener('progress', function()
+		{
+			self.speedIndicator_.textContent = self.rmng();
+		});
 		this.videoEl_.addEventListener('ratechange', function()
 		{
 			self.speedIndicator_.textContent = self.rmng();
